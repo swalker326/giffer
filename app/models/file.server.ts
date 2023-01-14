8a7995f3cf242dbac65300a23a8f937442dbe8b7
Shane <swalker@lemongrasscloud.com>
2023-01-14 08:49:08 -0600

    removed some logs

--- app/models/file.server.ts
+++ app/models/file.server.ts
@@ -4,16 +4,9 @@ export async function convertMovToGif(
   inputFile: Blob,
   outputFile = "output.gif"
 ) {
-  console.log("typeof inputFile", typeof inputFile);
-  // const gifOptions = {
-  //   fps: 30,
-  //   scale: 0.5,
-  //   duration: 10,
-  // };
   try {
     console.log(`Converting ${inputFile.name} to ${outputFile}...`);
     const gif = await movToGif(inputFile);
-    // console.log("Gif in file.server.ts: ", gif);
     return gif;
   } catch (err) {
     console.error(`An error occurred: ${err}`);
