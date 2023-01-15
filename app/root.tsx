dc0e192aa08644de313c1acf62501ddc98adda70
Shane <swalker@lemongrasscloud.com>
2023-01-14 21:58:27 -0600

    Updated icon and tab name

--- app/root.tsx
+++ app/root.tsx
@@ -17,7 +17,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Remix Notes",
+  title: "Giffer",
   viewport: "width=device-width,initial-scale=1",
 });
 
