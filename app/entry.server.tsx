4d8ed7e887ac711c092a8ef63a1a79659de868b6
Shane <swalker@lemongrasscloud.com>
2023-01-16 11:48:31 -0600

    implemented correct error handleing for server.

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -4,9 +4,7 @@ import { Response } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
 import isbot from "isbot";
 import { renderToPipeableStream } from "react-dom/server";
-import { useLocation, useMatches } from "@remix-run/react";
 import * as Sentry from "@sentry/remix";
-import { useEffect } from "react";
 
 const ABORT_DELAY = 5000;
 
@@ -56,15 +54,6 @@ export default function handleRequest(
 }
 
 Sentry.init({
-  dsn: "https://a8ee3e0c23e34c5b9991e3db0ac009c8:00ba9d8599a6479fa78914b9f989b123@o4504515201007616.ingest.sentry.io/4504515201925120",
+  dsn: "https://examplePublicKey@o0.ingest.sentry.io/0",
   tracesSampleRate: 1,
-  integrations: [
-    new Sentry.BrowserTracing({
-      routingInstrumentation: Sentry.remixRouterInstrumentation(
-        useEffect,
-        useLocation,
-        useMatches
-      ),
-    }),
-  ],
 });
