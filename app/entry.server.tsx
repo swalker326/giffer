d32b0826a5d9fdf6b367ca6d2a1d4ef0c64de8b6
Shane <shane@swalker.dev>
2024-08-01 23:08:27 -0500

    okay sorted biome was using latest on the server and 1.5 locally.

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -6,8 +6,8 @@
 
 import { PassThrough } from "node:stream";
 
-import type { AppLoadContext, EntryContext } from "@remix-run/node";
 import { createReadableStreamFromReadable } from "@remix-run/node";
+import type { AppLoadContext, EntryContext } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
 import isbot from "isbot";
 import { renderToPipeableStream } from "react-dom/server";
@@ -30,13 +30,13 @@ export default function handleRequest(
 				responseStatusCode,
 				responseHeaders,
 				remixContext,
-		  )
+			)
 		: handleBrowserRequest(
 				request,
 				responseStatusCode,
 				responseHeaders,
 				remixContext,
-		  );
+			);
 }
 console.log("Done");
 function handleBotRequest(
