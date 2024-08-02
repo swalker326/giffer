169334ef2066267efd2c544ab30da98ccb155e90
Shane <shane@swalker.dev>
2024-08-01 23:31:53 -0500

    moved styles? was relative import broken?

--- app/root.tsx
+++ app/root.tsx
@@ -1,44 +1,3 @@
-// import type { LinksFunction, MetaFunction } from "@remix-run/node";
-// import {
-// 	Links,
-// 	LiveReload,
-// 	Meta,
-// 	Outlet,
-// 	Scripts,
-// 	ScrollRestoration,
-// } from "@remix-run/react";
-
-// import tailwindStylesheetUrl from "./styles/tailwind.css";
-// import { withSentry } from "@sentry/remix";
-
-// export const links: LinksFunction = () => {
-// 	return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
-// };
-
-// export const meta: MetaFunction = () => ({
-// 	charset: "utf-8",
-// 	title: "Giffer",
-// 	viewport: "width=device-width,initial-scale=1",
-// });
-
-// function App() {
-// 	return (
-// 		<html lang="en" className="h-full">
-// 			<head>
-// 				<Meta />
-// 				<Links />
-// 			</head>
-// 			<body className="h-full">
-// 				<Outlet />
-// 				<ScrollRestoration />
-// 				<Scripts />
-// 				<LiveReload />
-// 			</body>
-// 		</html>
-// 	);
-// }
-// export default withSentry(App);
-
 import {
 	Links,
 	Meta,
@@ -46,7 +5,7 @@ import {
 	Scripts,
 	ScrollRestoration,
 } from "@remix-run/react";
-import "./styles/tailwind.css";
+import "~/tailwind.css";
 
 export function Layout({ children }: { children: React.ReactNode }) {
 	return (
